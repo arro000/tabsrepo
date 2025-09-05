4d2bc24034ff07880b59598439149c08f3c2983d
Andrea Arrighi <arrighi000@gmail.com>
2025-09-05 08:12:00 +0000

    Remove English and Italian localization files and update dependencies in pubspec.yaml and pubspec.lock
    - Deleted localization files for English and Italian languages.
    - Updated the following dependencies in pubspec.yaml:
      - archive: updated from ^3.4.9 to ^4.0.7
      - flutter_midi_command: updated from ^0.4.15 to ^0.5.3
      - flutter_typeahead: updated from ^4.8.0 to ^5.2.0
      - flutter_lints: updated from ^3.0.0 to ^6.0.0
    - Updated pubspec.lock to reflect the changes in dependencies.

--- android/app/src/main/java/io/flutter/plugins/GeneratedPluginRegistrant.java
+++ android/app/src/main/java/io/flutter/plugins/GeneratedPluginRegistrant.java
@@ -46,6 +46,11 @@ public final class GeneratedPluginRegistrant {
       Log.e(TAG, "Error registering plugin sqflite_android, com.tekartik.sqflite.SqflitePlugin", e);
     }
     try {
+      flutterEngine.getPlugins().add(new com.navideck.universal_ble.UniversalBlePlugin());
+    } catch (Exception e) {
+      Log.e(TAG, "Error registering plugin universal_ble, com.navideck.universal_ble.UniversalBlePlugin", e);
+    }
+    try {
       flutterEngine.getPlugins().add(new io.flutter.plugins.urllauncher.UrlLauncherPlugin());
     } catch (Exception e) {
       Log.e(TAG, "Error registering plugin url_launcher_android, io.flutter.plugins.urllauncher.UrlLauncherPlugin", e);
